bdb37ba6c354cf1363fe4e697274bd17fa721060
BiggusMaximus <jahekeraton164@gmail.com>
2025-10-05 21:26:48 +0700

    Spanning, Linear Independence, Subspace

--- Latihan Program/main.py
+++ Latihan Program/main.py
@@ -11,44 +11,53 @@ plt.rcParams.update({
     'text.latex.preamble': r'\usepackage{amsfonts}\usepackage{amsmath}',
     "font.family": "Times New Roman"
 })
-
-P1 = np.array([2, 8, 4])
-P2 = np.array([7, 10, -8])
-
-# Define vector from P1 to P2
-v = P2 - P1   # (5, 6, -12)
-
-# 3D Plot
-fig = plt.figure(figsize=(8,8))
+# Create 3D plot
+fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(111, projection='3d')
 
-# --- Draw vector from origin to P1
-ax.quiver(0, 0, 0, P1[0], P1[1], P1[2], color="blue", arrow_length_ratio=0.1, linewidth=2)
-ax.text(P1[0]+0.3, P1[1], P1[2], r"$\vec{OP}_1$", fontsize=12, color="blue")
+# Unit Vector
+anchor = 10
+ax.plot([-anchor, anchor], [0, 0], [0, 0]  , color="black", linestyle="solid", linewidth=0.1, zorder=10)  # X-axis
+ax.plot([0, 0], [-anchor, anchor], [0, 0]  , color="black", linestyle="solid", linewidth=0.1, zorder=10)  # Y-axis
+ax.plot([0, 0], [0, 0], [-anchor, anchor]          , color="black", linestyle="solid", linewidth=0.1, zorder=10)       # Z-axis
 
-# --- Draw vector from origin to P2
-ax.quiver(0, 0, 0, P2[0], P2[1], P2[2], color="red", arrow_length_ratio=0.1, linewidth=2)
-ax.text(P2[0]+0.3, P2[1], P2[2], r"$\vec{OP}_2$", fontsize=12, color="red")
+i = np.array([1, 0, 0])
+j = np.array([0, 1, 0])
+k = np.array([0, 0, 1])
+ax.quiver(0, 0, 0, *i, color="r", arrow_length_ratio=0.1, linewidth=0.5, label=rf'$i=\begin{{bmatrix}} {i[0]} & {i[1]} & {i[2]} \end{{bmatrix}}$')
+ax.quiver(0, 0, 0, *j, color="g", arrow_length_ratio=0.1, linewidth=0.5, label=rf'$j=\begin{{bmatrix}} {j[0]} & {j[1]} & {j[2]} \end{{bmatrix}}$')
+ax.quiver(0, 0, 0, *k, color="b", arrow_length_ratio=0.1, linewidth=0.5, label=rf'$k=\begin{{bmatrix}} {k[0]} & {k[1]} & {k[2]} \end{{bmatrix}}$')
 
-# --- Draw vector from P1 to P2
-ax.quiver(P1[0], P1[1], P1[2], v[0], v[1], v[2], color="green", arrow_length_ratio=0.1, linewidth=2)
-ax.text(P1[0]+v[0]/2, P1[1]+v[1]/2, P1[2]+v[2]/2, r"$\vec{v} = \overrightarrow{P_1P_2}$", fontsize=12, color="green")
+# New Basis 
+v1 = np.array([1, 1, 0])
+v2 = np.array([0, 1, 1])
+v3 = np.array([1, 0, 1])
+anchor = 10
+ax.plot(
+    [-anchor * v1[0], anchor*v1[0]], 
+    [-anchor * v1[1], anchor*v1[1]],
+    [-anchor * v1[2], anchor*v1[2]], 
+    color="black", linestyle="solid", linewidth=0.8, zorder=10)  
+ax.plot(
+    [-anchor * v2[0], anchor*v2[0]], 
+    [-anchor * v2[1], anchor*v2[1]],
+    [-anchor * v2[2], anchor*v2[2]], 
+    color="black", linestyle="solid", linewidth=0.8, zorder=10)  
+ax.plot(
+    [-anchor * v3[0], anchor*v3[0]], 
+    [-anchor * v3[1], anchor*v3[1]],
+    [-anchor * v3[2], anchor*v3[2]], 
+    color="black", linestyle="solid", linewidth=0.8, zorder=10)  
 
-# Set axes limits
-ax.set_xlim([-1, 8])
-ax.set_ylim([-2, 8])
-ax.set_zlim([-12, 5])
+ax.quiver(0, 0, 0, *v1, color="lime", arrow_length_ratio=0.1, linewidth=2, label=rf'$v_1=\begin{{bmatrix}} {i[0]} & {i[1]} & {i[2]} \end{{bmatrix}}$')
+ax.quiver(0, 0, 0, *v2, color="royalblue", arrow_length_ratio=0.1, linewidth=2, label=rf'$v_2=\begin{{bmatrix}} {j[0]} & {j[1]} & {j[2]} \end{{bmatrix}}$')
+ax.quiver(0, 0, 0, *v3, color="yellow", arrow_length_ratio=0.1, linewidth=2, label=rf'$v_3=\begin{{bmatrix}} {k[0]} & {k[1]} & {k[2]} \end{{bmatrix}}$')
 
-# Axis labels
-ax.set_xlabel("X", fontsize=12)
-ax.set_ylabel("Y", fontsize=12)
-ax.set_zlabel("Z", fontsize=12)
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("z")
+ax.set_aspect('equal')
+ax.grid(False)
+ax.legend()
 
-# Draw Cartesian axes
-anchor = 8
-ax.plot([-anchor, anchor], [0,0], [0,0], color="black", linewidth=0.8)  # X-axis
-ax.plot([0,0], [-anchor, anchor], [0,0], color="black", linewidth=0.8)  # Y-axis
-ax.plot([0,0], [0,0], [-anchor, anchor], color="black", linewidth=0.8)  # Z-axis
-ax.grid(False)  
-ax.set_title(v)
 plt.show()
